4f29094a448e44719312075ffea0c42592cbad54
Supreeth03 <supreeth0904@gmail.com>
2024-04-02 15:16:12 +0530

    function file for operators

---
+++ operators.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    print(a + b)
+
+
+# In[2]:
+def sub(a, b):
+    print(a - b)
+
+
+# In[3]:
+def mul(a, b):
+    print(a * b)
+
+
+# In[4]:
+def div(a, b):
+    print(a / b)
+
+
+def mod(a, b):
+    print(a % b)
